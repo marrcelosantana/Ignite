efccccee9e689fb13e2df400d2ef8084f4a32b0c
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-22 10:20:39 -0300

    useState

--- Chapter I/01-github-explorer/src/App.jsx
+++ Chapter I/01-github-explorer/src/App.jsx
@@ -1,7 +1,12 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App(){
   return(
-    <h1>Hello ReactJS!</h1>
+    <>
+      <RepositoryList/>
+      <Counter />
+    </>
   );
 }
