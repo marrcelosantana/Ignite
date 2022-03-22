8de766a627654c2e86b0bac2ab7010668137546d
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-22 19:23:56 -0300

    add types

--- Chapter I/02-github-explorer-typescript/src/components/RepositoryList.tsx
+++ Chapter I/02-github-explorer-typescript/src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useState, useEffect } from "react";
 
+interface Repository { 
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -15,7 +21,7 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        {repositories.map((repository) => {
+        {repositories.map((repository: Repository) => {
           return <RepositoryItem key={repository.name} repository={repository}/>
         })}
       </ul>
