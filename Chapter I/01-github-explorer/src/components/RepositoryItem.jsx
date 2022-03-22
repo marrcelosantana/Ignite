b87c0feaae98572bc7cee329c6b5fd364c662048
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-22 13:47:37 -0300

    finish

--- Chapter I/01-github-explorer/src/components/RepositoryItem.jsx
+++ Chapter I/01-github-explorer/src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export function RepositoryItem(props) {
     <li>
       <strong>{ props.repository.name }</strong>
       <p>{ props.repository.description }</p>
-      <a href={ props.repository.link }>Acessar repositórios</a>
+      <a href={ props.repository.html_url }>Acessar repositórios</a>
     </li>
   );
 }
