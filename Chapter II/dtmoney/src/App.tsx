cd4242ce90d6d52c207e51d11edcd2be17dd8b26
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-23 16:35:54 -0300

    add modal

--- Chapter II/dtmoney/src/App.tsx
+++ Chapter II/dtmoney/src/App.tsx
@@ -1,13 +1,31 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
-import { GlobalStyle } from "./styles/global"
+import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleClosenNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
   return (
     <>
-      <GlobalStyle/>
-      <Header/>
-      <Dashboard/>
+      <GlobalStyle />
+      <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal} />
+      <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleClosenNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
     </>
   );
 }
