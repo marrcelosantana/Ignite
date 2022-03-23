f0064c680e4a15e02034aab13ccc2204f1836db5
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-23 10:23:09 -0300

    init summary

--- Chapter II/dtmoney/src/App.tsx
+++ Chapter II/dtmoney/src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Summary } from "./components/Summary";
 import { GlobalStyle } from "./styles/global"
 
 export function App() {
@@ -6,6 +7,7 @@ export function App() {
     <>
       <GlobalStyle/>
       <Header/>
+      <Summary/>
     </>
   );
 }
