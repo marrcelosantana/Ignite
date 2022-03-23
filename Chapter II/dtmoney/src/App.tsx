503ea7a724837ae1b2d2959d0d0d8ac3b168ac44
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-23 16:09:34 -0300

    add services

--- Chapter II/dtmoney/src/App.tsx
+++ Chapter II/dtmoney/src/App.tsx
@@ -1,7 +1,6 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global"
-import { TransactionsTable } from "./components/TransactionsTable";
 
 export function App() {
   return (
