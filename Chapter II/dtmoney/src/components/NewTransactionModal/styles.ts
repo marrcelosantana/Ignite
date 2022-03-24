a5dba623f729fc5787294ca57e48529b675451d2
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-24 12:18:46 -0300

    finish modal stylization

--- Chapter II/dtmoney/src/components/NewTransactionModal/styles.ts
+++ Chapter II/dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -51,31 +51,42 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: transparent;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition: border-color 0.2s;
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
+}
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
 
-    &:hover{
-      border-color: ${ darken(0.1, '#d7d7d7') };
-    }
+  background: ${(props) => props.isActive ? transparentize(0.8, colors[props.activeColor]) : 'transparent'};
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
   }
 `;
