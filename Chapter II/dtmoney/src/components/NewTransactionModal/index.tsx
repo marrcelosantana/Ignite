f0865cc12455716ae9e813327146136c6ae79a9d
marrcelosantana <marcelosantana@alu.ufc.br>
2022-03-24 11:08:30 -0300

    add modal html

--- Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
+++ Chapter II/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -9,8 +10,16 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar Transação</h2>
+      <Container>
+        <h2>Cadastrar Transação</h2>
+        <input placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- Chapter II/dtmoney/src/styles/global.ts
+++ Chapter II/dtmoney/src/styles/global.ts
@@ -19,10 +19,10 @@ export const GlobalStyle = createGlobalStyle`
   }
   html{
     @media (max-width: 1080px){
-        font-size: 93.75%    // 15px
+      font-size: 93.75%    // 15px
     }
     @media (max-width: 720px){
-        font-size: 87.5%;   // 14px
+      font-size: 87.5%;   // 14px
     }
   }
   body{
@@ -43,5 +43,26 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background-color: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
   }
 `;
