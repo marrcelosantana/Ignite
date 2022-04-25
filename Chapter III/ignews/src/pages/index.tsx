a90f78c08def61a604f8b45cb50a557eefb1df8d
marrcelosantana <marcelosantana@alu.ufc.br>
2022-04-25 20:22:17 -0300

    first commit on Chapter III

---
+++ Chapter III/ignews/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.News</title>
+      </Head>
+      
+      <h1>Hello World!</h1>
+    </>
+  );
+}
