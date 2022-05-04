21430b9257341d80c174af1fa97d3f2bef3beb5b
marrcelosantana <marcelosantana@alu.ufc.br>
2022-05-03 21:21:10 -0300

    updates

--- Chapter III/ignews/src/components/Header/index.tsx
+++ Chapter III/ignews/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="IG News" />
+        <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
