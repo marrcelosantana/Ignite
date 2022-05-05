09b68165a16989998ef4856f6c3334334aabddad
marrcelosantana <marcelosantana@alu.ufc.br>
2022-05-05 19:38:56 -0300

    Insert Home styles

--- Chapter III/ignews/src/pages/index.tsx
+++ Chapter III/ignews/src/pages/index.tsx
@@ -11,9 +11,11 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
           <p>
-            Get access to all the publications <br/>
+            Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
         </section>
